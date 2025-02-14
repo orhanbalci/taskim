9538c94de060dfcd8c5a1f36361467d63d8cabd1
Rohan Adwankar <39285979+RohanAdwankar@users.noreply.github.com>
2025-02-14 01:46:53 -0800

    dangerous death script

--- src/index.js
+++ src/index.js
@@ -2,8 +2,8 @@ import React from 'react';
 import ReactDOM from 'react-dom/client'; // Note the new import for React 18
 import App from './App';
 import './index.css';
+import PouchDB from 'pouchdb';
 
-// Import DndProvider and the HTML5Backend
 import { DndProvider } from 'react-dnd';
 import { HTML5Backend } from 'react-dnd-html5-backend';
 
@@ -14,3 +14,15 @@ root.render(
     <App />
   </DndProvider>
 );
+
+window.clearPouchDBData = () => {
+  const db = new PouchDB('task_manager_data');
+  db.destroy()
+    .then(() => {
+      console.log("Database destroyed. All data has been deleted.");
+      window.location.reload(); // Reload to initialize a fresh database
+    })
+    .catch((err) => {
+      console.error("Error deleting database:", err);
+    });
+};
