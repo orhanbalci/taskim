7e315afd07eab9f3feff09d1ce304b6c852f84fb
Rohan Adwankar <39285979+RohanAdwankar@users.noreply.github.com>
2025-06-13 11:29:54 -0700

    refactor repo
    this repo used to contain boht the js and rust apps
    so im moving this reop to all rust and put the js app in a seperate repo

--- src/App.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import TaskManagerCalendar from './TaskManagerCalendar';
-import './index.css'; 
-
-function App() {
-  return <TaskManagerCalendar />;
-}
-
-export default App;
